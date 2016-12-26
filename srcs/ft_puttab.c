c480bbe29fb0c390d0f3af1fbcc352ea77da3169
Clurquiz <clementine.urquizar@gmail.com>
2016-12-26 19:03:21 +0100

    ajout \n et NULL

--- srcs/ft_puttab.c
+++ srcs/ft_puttab.c
@@ -1,18 +1,21 @@
 #include "libft.h"
 
 /*
-** Affiche un double pointeur sur char
-** sachant que le dernier pointeur sur char du tableau est NULL
-*/
+ ** Affiche un double pointeur sur char
+ ** sachant que le dernier pointeur sur char du tableau est NULL
+ */
 
 void	ft_puttab(char **tab)
 {
 	int		i;
 
 	i = 0;
-	while (tab[i])
+	if (tab)
 	{
-		ft_putstr(tab[i]);
-		i++;
+		while (tab[i])
+		{
+			ft_putendl(tab[i]);
+			i++;
+		}
 	}
 }
