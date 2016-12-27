de2cff3794b638030166fa386da5773b8ef451f5
Clurquiz <clementine.urquizar@gmail.com>
2016-12-27 11:21:34 +0100

    chgt comment

--- srcs/ft_puttab.c
+++ srcs/ft_puttab.c
@@ -1,9 +1,9 @@
 #include "libft.h"
 
 /*
- ** Affiche un double pointeur sur char
- ** sachant que le dernier pointeur sur char du tableau est NULL
- */
+** Affiche un double pointeur sur char
+** sachant que le dernier pointeur sur char du tableau est NULL
+*/
 
 void	ft_puttab(char **tab)
 {
